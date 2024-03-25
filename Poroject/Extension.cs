2eaa650b72469e5dd284499678d7787e26d5120b
d3baf <korobka.yaroslav@yandex.ru>
2024-03-25 19:34:24 +0300

    класс расширение print

---
+++ Poroject/Extension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poroject
+{
+    static class Extension
+    {
+        public static T[] Print <T>(this T[] array)
+        {
+            foreach(var item in array)
+                Console.WriteLine(item + "\t");
+            Console.WriteLine();
+            return array;
+        }
+        public static T Print<T>(this T item)
+        {
+            Console.WriteLine(item);
+            return item;
+        }
+    }
+}
