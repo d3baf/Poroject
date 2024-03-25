f5346804d2b32b4dcaed2704da7c30f374f23178
d3baf <korobka.yaroslav@yandex.ru>
2024-03-25 20:39:51 +0300

    рандом

---
+++ Poroject/Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poroject
+{
+    static class Program1
+    {
+        public static void main()
+        {
+
+            int[] arr = new int[10]
+                .Select(x => new Random().Next(100)).ToArray().Print();
+            int[] arr1 = arr.OrderBy(x => x).ToArray();
+            int[] arr2 = arr.OrderByDescending(x=>x).ToArray();
+            if (arr.SequenceEqual(arr1))
+                "OrderBy".Print();
+            else if (arr.SequenceEqual(arr2))
+                "OrderByDescending".Print();
+            else
+                "random".Print();
+        }
+    }
+}
